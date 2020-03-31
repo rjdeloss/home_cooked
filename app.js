ec000bb7ced9a51922f78c488baaa77e90949aba
Droool <rjdeloss@gmail.com>
2020-03-31 18:44:51 -0400

    connect mongoDB to app

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 // Server
 const app = express();
 const port = process.env.port || 3000;
+const db = require('./config/keys').mongoURI;
+
+mongoose
+	.connect(db, { useNewUrlParser: true })
+	.then(() => console.log('connected to database successfully'))
+	.catch(err => console.log(err));
 
 app.listen(port, () => console.log(`server is running on port:${port}`));
 
