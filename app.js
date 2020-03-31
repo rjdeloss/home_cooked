a52a115e6b4a74289b765ad353e5207ee1fc2cf8
Droool <rjdeloss@gmail.com>
2020-03-31 18:27:14 -0400

    create express server

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-const express = require("express");
+const express = require('express');
+
+// Server
+const app = express();
+const port = process.env.port || 3000;
+
+app.listen(port, () => console.log(`server is running on port:${port}`));
+
+// Routes
+app.get('/', (req, res) => res.send('Hello World'));
