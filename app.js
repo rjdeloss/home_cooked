3b4d98b7fc0e0f2864610c80ecfc8c89fdc9c7bf
Droool <rjdeloss@gmail.com>
2020-04-01 23:18:48 -0400

    begin routes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 const port = process.env.port || 3000;
 const db = require('./config/keys').mongoURI;
 
+
 mongoose
 	.connect(db, { useNewUrlParser: true })
 	.then(() => console.log('connected to database successfully'))
@@ -14,4 +15,4 @@ mongoose
 app.listen(port, () => console.log(`server is running on port:${port}`));
 
 // Routes
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', ( req, res) => res.send('Hello World'));
